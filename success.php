8c4e832d3f2309f3f99dcd0679b88524db532b38
Abimanyu <gilarwaluyo@gmail.com>
2024-11-24 14:37:43 +0700

    menambahkan halaman success

---
+++ success.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Smart Guest Book on Chip</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        body {
+            background-image: url("img/gedung kuliah terpadu.jpg");
+            background-size: cover;
+            background-position: center;
+            background-attachment: fixed;
+            background-repeat: no-repeat;
+        }
+
+        body::before {
+            content: "";
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background: rgba(255, 255, 255, 0.7);
+            z-index: -1;
+        }
+
+        .fullscreen-center {
+            height: 100vh; /* Mengatur tinggi container penuh */
+        }
+    </style>
+</head>
+
+<body class="d-flex flex-column justify-content-center align-items-center fullscreen-center">
+  <h1>Thank you for registering</h1>
+  <img src="img/centang.png" alt="Centang" class="img-fluid" style="max-width: 300px; max-height: 300px;">
+  <h1>ID : </h1>
+</body>
+
+</html>
